786d845286d8857d08e314ff950abdc9bb20b04c
nourseen1 <121319177+nourseen1@users.noreply.github.com>
2022-12-24 02:22:45 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+namespace Project_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num, i, m, first, last;
+
+
+            Console.Write("Input First number : ");
+            first = int.Parse(Console.ReadLine());
+            Console.Write("Input Last number : ");
+            last = int.Parse(Console.ReadLine());
+            Console.Write("The prime numbers between {0} and {1} are : \n", first, last);
+
+
+
+
+            for (num = first; num <= last; num++)
+            {
+                m = 0;
+
+
+
+                for (i = 2; i <= num / 2; i++)
+                {
+                    if (num % i == 0)
+                    {
+                        m++;
+                        break;
+                    }
+                }
+
+                if (m == 0 && num != 1)
+                    Console.WriteLine(num);
+            }
+
+
+
+            Console.ReadLine();
+        }
+    }
+}
+    
+
